55664880ae7ca358210d8a080a1039d914329233
Aqib Malik <malik.ab454@gmail.com>
2022-05-27 16:24:41 +0500

    Optimal Solution for word-squares problem added

--- word-squares/code.py
+++ word-squares/code.py
@@ -1,36 +1,35 @@
 
-
-# Check if every word in the list contains same number of letters
-def is_sequence_square(words):
-    for word in words:
-        if len(words[0]) != len(word):
-            return False
-    return True
-
 def find_word_squares(sequence):
-    word_squares = []
     word_length = len(sequence[0])
-    sequence_length = len(sequence)
-    possibles = []
-    for i in range(sequence_length):
-        temp = []
-        for index, letter in enumerate(sequence[i]):
-            print("INDEX: ", index)
-            for j in range(sequence_length):
-                if i == j:
-                    continue
-                if letter == sequence[j][0]:
-                    temp.append(sequence[j])
-                    print(index, letter, sequence[j][0], sequence[i], temp)
-        possibles.append(temp)
-    print(possibles)
+    word_squares = []
+    for word in sequence:
+        current_square = []
+        current_square.append(word)
+        dump = {}
 
+        i = 1
+        while i < word_length:
+            found = False
+            prefix = ""
+            for s in current_square:
+                prefix += s[i:i+1]
+            for j in range(len(sequence)):
+                if word != sequence[j] and dump.get(sequence[j], None) is None:
+                    if prefix == sequence.__getitem__(j)[0:i]:
+                        found = True
+                        current_square.append(sequence[j])
+                        break
+            if(not found and len(current_square) > 1):
+                i = i - 2
+                dump[current_square.pop()] = 1
+            i += 1
 
+        if len(current_square) > 0:
+            if len(current_square) == len(current_square[0]):
+                word_squares.append(current_square)
+    return word_squares
 
 
 if __name__ == '__main__':
-    INPUT = ["AREA", "BALL", "DEAR", "LADY", "LEAD", "YARD"]
-    if(is_sequence_square(INPUT)):
-        find_word_squares(INPUT)
-
-
+    INPUT = ["BALL", "ABLE", "AREA", "DEAR", "LEAD", "YARD", "LADY"]
+    print(find_word_squares(INPUT))
